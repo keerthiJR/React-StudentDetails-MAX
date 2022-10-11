cce903172e2f53e8ef7390f593a9eb91ed3cfa65
keerthiJR <keerthigandhi1552001@gmail.com>
2022-10-11 22:29:06 +0530

    added error modular UI Component

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import Card from '../UI/Card';
 import Button from '../UI/Button';
 import classes from './AddUser.module.css';
+import ErrorModal from '../UI/ErrorModal';
 
 const AddUser = (props) => {
 
@@ -12,42 +13,45 @@ const AddUser = (props) => {
 
   const addUserHandler = (event) => {
     event.preventDefault();
-    if(enteredUsername.trim().length === 0 || enteredDesignation.trim().length === 0 || enteredAge.trim().length === 0){
+    if (enteredUsername.trim().length === 0 || enteredDesignation.trim().length === 0 || enteredAge.trim().length === 0) {
       return;
     }
-    if(+enteredAge<0){
+    if (+enteredAge < 0) {
       return;
     }
-    props.adduser(enteredUsername,enteredAge, enteredDesignation)
+    props.adduser(enteredUsername, enteredAge, enteredDesignation)
     setEnteredAge('');
     setEnteredUsername('')
     setEnteredDesignation('')
   };
 
-  const usernameChangeHandler = (event)=>{
+  const usernameChangeHandler = (event) => {
     setEnteredUsername(event.target.value)
   }
 
-  const ageChangeHandler = (event)=>{
+  const ageChangeHandler = (event) => {
     setEnteredAge(event.target.value)
   }
 
-  const designationChangeHandler = (event)=>{
+  const designationChangeHandler = (event) => {
     setEnteredDesignation(event.target.value)
   }
 
   return (
-    <Card className={classes.input}>
-      <form onSubmit={addUserHandler}>
-        <label htmlFor="username">Student Name</label>
-        <input id="username" type="text" value={enteredUsername} onChange={usernameChangeHandler}/>
-        <label htmlFor="age">Age (Years)</label>
-        <input id="age" type="number" value={enteredAge} onChange={ageChangeHandler}/>
-        <label htmlFor="username">Designation</label>
-        <input id="designation" type="text" value={enteredDesignation} onChange={designationChangeHandler}/>
-        <Button type="submit">Add Student</Button>
-      </form>
-    </Card>
+    <div>
+      <ErrorModal title="An error occured" message="something wwent wrong"/>
+      <Card className={classes.input}>
+        <form onSubmit={addUserHandler}>
+          <label htmlFor="username">Student Name</label>
+          <input id="username" type="text" value={enteredUsername} onChange={usernameChangeHandler} />
+          <label htmlFor="age">Age (Years)</label>
+          <input id="age" type="number" value={enteredAge} onChange={ageChangeHandler} />
+          <label htmlFor="username">Designation</label>
+          <input id="designation" type="text" value={enteredDesignation} onChange={designationChangeHandler} />
+          <Button type="submit">Add Student</Button>
+        </form>
+      </Card>
+    </div>
   );
 };
 
