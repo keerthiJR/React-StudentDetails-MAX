1478c163f8e10c4dec310485d9c0fe4b90a81881
keerthiJR <keerthigandhi1552001@gmail.com>
2022-10-11 22:46:29 +0530

    final update V1.0

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -2,13 +2,9 @@ import Button from "./Button";
 import Card from "./Card";
 import classes from './ErrorModal.module.css'
 const ErrorModal = (props) => {
-
-    const ErrorModalhandler = () =>{
-        props.clickHandler()
-    }
     return (
         <div>
-            <div className={classes.backdrop} />
+            <div className={classes.backdrop} onClick={props.clickHandler}/>
             <Card className={classes.modal}>
                 <header className={classes.header}>
                     <h2>{props.title}</h2>
@@ -17,7 +13,7 @@ const ErrorModal = (props) => {
                     <p>{props.message}</p>
                 </div>
                 <footer className={classes.actions}>
-                    <Button onClick={ErrorModalhandler}>okay</Button>
+                    <Button onClick={props.clickHandler}>okay</Button>
                 </footer>
             </Card>
         </div>
