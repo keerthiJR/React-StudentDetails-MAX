b27784f079edcd1ac0c959f2215d3e3765388498
keerthiJR <keerthigandhi1552001@gmail.com>
2022-10-11 22:41:44 +0530

    added validations for Error Modal Component

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -2,6 +2,10 @@ import Button from "./Button";
 import Card from "./Card";
 import classes from './ErrorModal.module.css'
 const ErrorModal = (props) => {
+
+    const ErrorModalhandler = () =>{
+        props.clickHandler()
+    }
     return (
         <div>
             <div className={classes.backdrop} />
@@ -13,7 +17,7 @@ const ErrorModal = (props) => {
                     <p>{props.message}</p>
                 </div>
                 <footer className={classes.actions}>
-                    <Button>okay</Button>
+                    <Button onClick={ErrorModalhandler}>okay</Button>
                 </footer>
             </Card>
         </div>
--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -10,13 +10,22 @@ const AddUser = (props) => {
   const [enteredUsername, setEnteredUsername] = useState('');
   const [enteredAge, setEnteredAge] = useState('');
   const [enteredDesignation, setEnteredDesignation] = useState('');
+  const [error, setError] = useState();
 
   const addUserHandler = (event) => {
     event.preventDefault();
     if (enteredUsername.trim().length === 0 || enteredDesignation.trim().length === 0 || enteredAge.trim().length === 0) {
+      setError({
+        title: 'Invalid input',
+        message: 'Please enter a valid name, age and designation'
+      })
       return;
     }
     if (+enteredAge < 0) {
+      setError({
+        title: 'Invalid age',
+        message: 'Please enter a valid age > 0'
+      })
       return;
     }
     props.adduser(enteredUsername, enteredAge, enteredDesignation)
@@ -37,9 +46,13 @@ const AddUser = (props) => {
     setEnteredDesignation(event.target.value)
   }
 
+  const onClickErrorModalHander = ()=>{
+    setError('')
+  }
+
   return (
     <div>
-      <ErrorModal title="An error occured" message="something wwent wrong"/>
+      { error && <ErrorModal title={error.title} message={error.message} clickHandler={onClickErrorModalHander}/> }
       <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
           <label htmlFor="username">Student Name</label>
